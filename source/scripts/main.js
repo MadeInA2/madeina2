c4e49167535c33f4651b5577d84728288a65f93d
Sam Pierce Lolla <sampiercelolla@gmail.com>
2016-09-23 14:33:17 -0400

    basic hello world calendar page w/ fullcalendar

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,1 +1,69 @@
-$('.sticky').Stickyfill();
+$(document).ready(function() {
+
+	// sticky sidebar on Community page
+	$('.sticky').Stickyfill();
+
+	// calendar on Events page
+	$('#calendar').fullCalendar({
+		defaultDate: '2016-09-12',
+		editable: true,
+		eventLimit: true, // allow "more" link when too many events
+		events: [
+			{
+				title: 'All Day Event',
+				start: '2016-09-01'
+			},
+			{
+				title: 'Long Event',
+				start: '2016-09-07',
+				end: '2016-09-10'
+			},
+			{
+				id: 999,
+				title: 'Repeating Event',
+				start: '2016-09-09T16:00:00'
+			},
+			{
+				id: 999,
+				title: 'Repeating Event',
+				start: '2016-09-16T16:00:00'
+			},
+			{
+				title: 'Conference',
+				start: '2016-09-11',
+				end: '2016-09-13'
+			},
+			{
+				title: 'Meeting',
+				start: '2016-09-12T10:30:00',
+				end: '2016-09-12T12:30:00'
+			},
+			{
+				title: 'Lunch',
+				start: '2016-09-12T12:00:00'
+			},
+			{
+				title: 'Meeting',
+				start: '2016-09-12T14:30:00'
+			},
+			{
+				title: 'Happy Hour',
+				start: '2016-09-12T17:30:00'
+			},
+			{
+				title: 'Dinner',
+				start: '2016-09-12T20:00:00'
+			},
+			{
+				title: 'Birthday Party',
+				start: '2016-09-13T07:00:00'
+			},
+			{
+				title: 'Click for Google',
+				url: 'http://google.com/',
+				start: '2016-09-28'
+			}
+		]
+	});
+
+});
