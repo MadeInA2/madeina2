25d79aab733c1a680cc2b1ef48c9249b0ec5d28c
Cam <cam@duo.com>
2016-10-23 21:08:57 -0400

    Use var instead of const for compatibility

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -3,15 +3,15 @@ $(document).ready(function() {
 	// sticky sidebar on Community page
 	$('.sticky').Stickyfill();
 
-	const base_events_url = "https://api.meetup.com/2/open_events";
-	const params = 'callback=?&sign=true&zip=48103';
-	const api_key = '1';
-	const feed_url = base_events_url + '?' + params + '&' + api_key;
+	var base_events_url = "https://api.meetup.com/2/open_events";
+	var params = 'callback=?&sign=true&zip=48103';
+	var api_key = '1';
+	var feed_url = base_events_url + '?' + params + '&' + api_key;
 	events = [];
 	$.getJSON(feed_url, function (data) {
 		for(var i=0; i<data.results.length; i++) {
 			let item = data.results[i];
-			const event = {
+			var event = {
 				title: item['name'],
 				start: new moment(item['time']),
 				url: item['event_url'],
