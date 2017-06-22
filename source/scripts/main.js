9cb92b6823d2f9dacf85c3f2c6a77f3332b4d6ed
Sam Pierce Lolla <sampiercelolla@gmail.com>
2017-06-22 12:16:52 -0400

    got calendar working
    - new lib async.js for multiple calls
    - calendar uses an older meetup.com API, so may break later
    - the old API keeps us from having to make a server to send keys though

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,53 +1,150 @@
-$(document).ready(function() {
+// --------------------------------
+// ** ATTENTION EVENT ORGANIZERS **
+// add your groups to this array!
+// find your group name by looking
+// at your meetup group's url, and
+// dont' forget the comma at the end
+// --------------------------------
+var meetups = [
+	'A2-CocoaHeads',
+	'A2-Cust-Experience',
+	'A2-Cust-Support-Success',
+	'A2-Experience-Design-with-Agile-Lean-UX',
+	'a2-game-designers',
+	'a2civictech',
+	'a2entrepreneurs',
+	'a2magento',
+	'a2newtech',
+	'Agile-Groupies',
+	'allhandsactive',
+	'Ann-Arbor-C-Meetup',
+	'Ann-Arbor-Chapter-of-the-American-Statistical-Association',
+	'Ann-Arbor-Design-Group',
+	'Ann-Arbor-Detroit-NLPers-A2D-NLP',
+	'Ann-Arbor-Detroit-Startup-Meetup',
+	'Ann-Arbor-FinTech-Meetup',
+	'Ann-Arbor-Gophers',
+	'Ann-Arbor-Hadoop-User-Group-A2HUG',
+	'Ann-Arbor-Mobile-Development-Meetup',
+	'ann-arbor-php',
+	'Ann-Arbor-PyLadies',
+	'Ann-Arbor-R-User-Group',
+	'Ann-Arbor-Raspberry-Pi-Projects',
+	'Ann-Arbor-Scala-Enthusiasts-User-Group-Meeting',
+	'Ann-Arbor-Web-Analytics-Wednesday',
+	'AnnArborComputerSociety',
+	'Apache-Apex-Ann-Arbor-Meetup',
+	'AWS-Michigan',
+	'Balanced-Team-Ann-Arbor',
+	'connectedcar',
+	'Craftsman-Guild',
+	'Duo-Tech-Talks',
+	'Ember-A2D',
+	'Fanzoo-Learn-Something',
+	'Free-Code-Camp-Ann-Arbor',
+	'gdg-a3',
+	'Girl-Develop-It-Ann-Arbor',
+	'Machine-Learning-Data-Science-and-Industry',
+	'Meteor-Ann-Arbor',
+	'Michigan-Ethereum-Meetup',
+	'midrupal',
+	'MoMoa2',
+	'Predictive-Analytics-S-E-Michigan',
+	'Quantified-Self-Ann-Arbor',
+	'SEM-JS',
+]
 
-	// sticky sidebar on Community page
-	$('.sticky').Stickyfill();
+// ================================================================
 
-	var base_events_url = "https://api.meetup.com/2/open_events";
-	var params = 'callback=?&sign=true&zip=48103&fields=category';
-	var api_key = '1';
-	var feed_url = base_events_url + '?' + params + '&' + api_key;
-	events = [];
-	$.getJSON(feed_url, function (data) {
-        console.log(data.results.length + ' events returned');
+// takes a meetup group id and gets an array of Meetup API events for that group
+var getEventsForGroup = function(group, callback) {
+	$.ajax({
+		url: 'https://api.meetup.com/'+group+'/events',
+		jsonp: "callback",
+		dataType: "jsonp",
+		data: {
+			format: "json"
+		},
+		success: function(response) {
+			var events = response.data;
 
-		var printed = false;
-		for(var i=0; i<data.results.length; i++) {
-			// categories based on
-			// https://secure.meetup.com/meetup_api/console/?path=/2/categories
-			// We should be able to pass in category id as a parameter to the
-			// Meetup API, but it still returns all events, not just tech (34)
-			// events. I'll debug that more later.
-			if(data.results[i].group.category.id != 34) {
-				if(!printed) {
-					console.log('Removing events');
-					printed = true;
-				}
-				data.results.splice(i, 1);
-				i--;
-				continue;
+			// filter out dead meetup groups
+			if (events.length > 0) {
+				console.log('got '+events.length+' events for '+group);
+				callback(null, events);
+			} else {
+				callback(null, []);
 			}
-			let item = data.results[i];
-			var event = {
-				title: item['name'],
-				start: new moment(item['time']),
-				url: item['event_url'],
-			}
-			events.push(event);
 		}
+	});
+}
 
-		$('#calendar').fullCalendar({
-			defaultDate: moment().format('YYYY-MM-DD'),
-			editable: true,
-			eventLimit: true,
-			events: events,
-			navLinks: true,
-			eventClick: function(calEvent, jsEvent, view) {
-				if(event.url) {
-					window.open(event.url);
-					return false;
-				}
-			}
+// takes an array of events from the Meetup API and renders it to the DOM using FullCalendar
+var renderCalendar = function(events) {
+
+	// format Meetup API => FullCalendar
+	var events = events.map(function(event){
+		return({
+			title: event['name'],
+			start: new moment(event['time']),
+			url: event['link'],
 		});
 	});
+
+	// render with FullCalendar
+	$('#calendar').fullCalendar({
+		defaultDate: moment().format('YYYY-MM-DD'),
+		editable: true,
+		eventLimit: true,
+		events: events,
+		navLinks: true,
+		eventClick: function(calEvent, jsEvent, view) {
+			if (calEvent.url) {
+				window.open(calEvent.url);
+				return false;
+			}
+		}
+	});
+
+}
+
+$(document).ready(function() {
+
+	// COMMUNITY PAGE
+
+	if (window.location.pathname.includes('community')) {
+		// community page sticky sidebar
+		$('.sticky').Stickyfill();
+	}
+
+	// CALENDAR PAGE
+
+	// TODO - maybe get events by meetup category and zip?
+	// currently fails because it needs an API key and we have no server atm
+	// Cam says...
+	// categories based on
+	// https://secure.meetup.com/meetup_api/console/?path=/2/categories
+	// We should be able to pass in category id as a parameter to the
+	// Meetup API, but it still returns all events, not just tech (34)
+	// events. I'll debug that more later.
+
+	if (window.location.pathname.includes('calendar')) {
+
+		// for each group, call getEventsForGroup and render the collective result
+		async.map(meetups, getEventsForGroup, function(err, events) {
+
+			if (err) {
+				alert('Whoops, couldn\'t get a list of events--try again soon!')
+				console.error('error', err)
+			}
+
+			// events is now an array of arrays of events (one array per group)
+			// need to flatten it to a single array of events
+			// https://stackoverflow.com/questions/10865025/merge-flatten-an-array-of-arrays-in-javascript
+			var events = [].concat.apply([], events)
+
+			renderCalendar(events)
+		})
+	}
+
 });
